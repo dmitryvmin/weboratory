6be26038a46ee90b7773ee9135c7e421edb9da23
Dmitry Minyaylov <dmitryminyaylov@ip-192-168-87-22.us-west-2.compute.internal>
2020-08-04 13:08:28 -0700

    WIP

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { graphql, ApolloProvider } from "react-apollo";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
 // App
 import "./index.scss";
@@ -9,6 +11,16 @@ import App from "./App";
 import history from "./utils/history";
 import { Auth0Provider } from "@utils/hooks/useAuth0";
 import { getEnv } from "@configs/env";
+
+const cache = new InMemoryCache();
+const link = createHttpLink({
+  uri: "https://weboratory.herokuapp.com/api/v1/events",
+});
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
 
 const onRedirectCallback = (appState: any) => {
   history.push(
@@ -19,17 +31,19 @@ const onRedirectCallback = (appState: any) => {
 };
 
 ReactDOM.render(
-  <Router>
-    <Auth0Provider
-      domain={getEnv("AUTH_DOMAIN")}
-      client_id={process.env.REACT_APP_AUTH_CLIENT_ID}
-      redirect_uri={window.location.origin}
-      onRedirectCallback={onRedirectCallback}
-    >
-      {/*<Auth0LockProvider>*/}
+  <ApolloProvider client={client}>
+    <Router>
+      <Auth0Provider
+        domain={getEnv("AUTH_DOMAIN")}
+        client_id={process.env.REACT_APP_AUTH_CLIENT_ID}
+        redirect_uri={window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+      >
+        {/*<Auth0LockProvider>*/}
         <App/>
-      {/*</Auth0LockProvider>*/}
-    </Auth0Provider>
-  </Router>,
+        {/*</Auth0LockProvider>*/}
+      </Auth0Provider>
+    </Router>
+  </ApolloProvider>,
   document.getElementById("root"),
 );
